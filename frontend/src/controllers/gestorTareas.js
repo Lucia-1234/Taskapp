2a2afc33c0d51128421f80310b6162b024eb16f5
Valentina Serrano <valentinaserrano120@gmail.com>
2026-04-29 16:24:40 -0500

    SE terminaron las validaciones requeridad por el modulo

--- frontend/src/controllers/gestorTareas.js
+++ frontend/src/controllers/gestorTareas.js
@@ -109,7 +109,7 @@ export const inicializarVistaTareas = () => {
     try {
       // Usamos el helper 'enviar' (método POST) para mandar los datos al servidor (JSONPlaceholder)
       const resultado = await enviar('task', nuevaTarea);
-
+      
       formTarea.reset();
 
     } catch (error) {
@@ -159,4 +159,5 @@ export const manejarCambioUsuario = (usuario) => {
     tasksListSection.classList.add('hidden');
   }
 };
+
 
